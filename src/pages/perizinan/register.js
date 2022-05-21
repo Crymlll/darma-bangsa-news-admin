12459802888831aa0f6e2c080afe20eae01c3c64
Aulia Rahman Zulfi <dragoncimol@gmail.com>
2022-05-21 09:21:33 +0700

    Update register.js

--- src/pages/perizinan/register.js
+++ src/pages/perizinan/register.js
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import {
   Box,
   Button,
@@ -16,10 +16,8 @@ import {
   FormControl,
   Select,
   InputLabel,
-
-} from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 import { useState, useEffect } from "react";
 import { db } from "../../firebase/firebase";
@@ -32,26 +30,23 @@ import {
   deleteDoc,
   doc,
   query,
-  where
-} from 'firebase/firestore';
-
-
+  where,
+} from "firebase/firestore";
 
 const Register = () => {
-
   const [text, onChangeText] = useState({
-    email: '',
-    kelas: '',
-    nama: '',
-    no_induk: '',
-    role: '',
-});
+    email: "",
+    kelas: "",
+    nama: "",
+    no_induk: "",
+    role: "",
+  });
 
-const clickHandler = (textInput) => {
+  const clickHandler = (textInput) => {
     return (value) => {
-        onChangeText({ ...text, [textInput]: value });
-    }
-}
+      onChangeText({ ...text, [textInput]: value });
+    };
+  };
 
   const [newName, setNewName] = useState("");
 
@@ -59,7 +54,7 @@ const clickHandler = (textInput) => {
   const [users2, setUsers2] = useState([]);
   const usersCollectionRef = collection(db, "users");
   const usersCollectionRef2 = collection(db, "users");
-  const addedDoc = collection(db, "perizinan")
+  const addedDoc = collection(db, "perizinan");
 
   const updateUser = async (id, age) => {
     const userDoc = doc(db, "users", id);
@@ -75,91 +70,73 @@ const clickHandler = (textInput) => {
   function getUsers2(kelas) {
     formik.values.kelas = kelas;
     const getUsers = async (kelas) => {
-      if(kelas==""){
-        const q2 = query(usersCollectionRef, where("role", "==", "siswa") )
+      if (kelas == "") {
+        const q2 = query(usersCollectionRef, where("role", "==", "siswa"));
         const hasil2 = await getDocs(q2);
         const semuaMurid = hasil2.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
         setUsers(semuaMurid);
-      }
-      else{
-        const q2 = query(usersCollectionRef, where("kelas", "==", kelas), where("role", "==", "siswa") )
+        const q = query(usersCollectionRef, where("role", "==", "wali kelas"));
+        const hasil = await getDocs(q);
+        const semuaGuru = hasil.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setUsers2(semuaGuru);
+      } else {
+        const q2 = query(
+          usersCollectionRef,
+          where("kelas", "==", kelas),
+          where("role", "==", "siswa")
+        );
         const hasil2 = await getDocs(q2);
         const semuaMurid = hasil2.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
         setUsers(semuaMurid);
+        const q = query(
+          usersCollectionRef,
+          where("kelas", "==", kelas),
+          where("role", "==", "wali kelas")
+        );
+        const hasil = await getDocs(q);
+        const semuaGuru = hasil.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setUsers2(semuaGuru);
       }
-    }
+    };
     getUsers(kelas);
   }
 
   useEffect(() => {
     const cobaTes = async () => {
-  
       // const q = query(usersCollectionRef, where('role', 'in', ['wali kelas', 'guru', 'guru konseling']));
       const q = query(usersCollectionRef2, where("role", "==", "guru konseling"));
-      const q2 = query(usersCollectionRef, where("role", "==", "siswa") )
+      const q2 = query(usersCollectionRef, where("role", "==", "siswa"));
       const hasil = await getDocs(q);
       const hasil2 = await getDocs(q2);
       const semuaMurid = hasil2.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-      const semuaGuru = hasil.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-      setUsers(semuaMurid)
-      setUsers2(semuaGuru)
-      console.log(users2)
-    }
-
+      const semuaGuru = hasil.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setUsers(semuaMurid);
+      setUsers2(semuaGuru);
+      console.log(users2);
+    };
 
     cobaTes();
-    
   }, []);
-
 
   const router = useRouter();
   const formik = useFormik({
     initialValues: {
-      alasan: '',
-      nama: '',
-      kelas: '',
-      kegiatan: '',
-      tanggal: '',
-      status: '',
+      alasan: "",
+      nama: "",
+      kelas: "",
+      kegiatan: "",
+      tanggal: "",
+      status: "",
       policy: false,
     },
     validationSchema: Yup.object({
-      permasalahan: Yup
-        .string()
-        .max(255)
-        .required(
-          'Permasalahan is required'),
-      guru: Yup
-      .string()
-      .max(255)
-      .required(
-        'Guru is required'),
-      kegiatan: Yup
-        .string()
-        .max(255)
-        .required(
-          'Kegiatan is required'),
-      nama: Yup
-        .string()
-        .max(255)
-        .required(
-          'Nama is required'),
-      tanggal: Yup
-      .string()
-      .max(255)
-      .required(
-        'Tanggal is required'),
-      status: Yup
-        .string()
-        .max(255)
-        .required(
-          'Status is required'),
-      policy: Yup
-        .boolean()
-        .oneOf(
-          [true],
-          'This field must be checked'
-        )
+      permasalahan: Yup.string().max(255).required("Permasalahan is required"),
+      guru: Yup.string().max(255).required("Guru is required"),
+      kegiatan: Yup.string().max(255).required("Kegiatan is required"),
+      nama: Yup.string().max(255).required("Nama is required"),
+      tanggal: Yup.string().max(255).required("Tanggal is required"),
+      status: Yup.string().max(255).required("Status is required"),
+      policy: Yup.boolean().oneOf([true], "This field must be checked"),
     }),
     onSubmit: () => {
       const createUser = async () => {
@@ -169,57 +146,42 @@ const clickHandler = (textInput) => {
           nama: formik.values.nama,
           kegiatan: formik.values.kegiatan,
           tanggal: formik.values.tanggal,
-          status: formik.values.status
+          status: formik.values.status,
         });
       };
 
-      createUser()
+      createUser();
 
-      router.push('/konseling');
-    }
+      router.push("/konseling");
+    },
   });
 
   return (
     <>
       <Head>
-        <title>
-          Perizinan Register
-        </title>
+        <title>Perizinan Register</title>
       </Head>
       <Box
         component="main"
         sx={{
-          alignItems: 'center',
-          display: 'flex',
+          alignItems: "center",
+          display: "flex",
           flexGrow: 1,
-          minHeight: '100%'
+          minHeight: "100%",
         }}
       >
         <Container maxWidth="sm">
-          <NextLink
-            href="/konseling"
-            passHref
-          >
-            <Button
-              component="a"
-              startIcon={<ArrowBackIcon fontSize="small" />}
-            >
+          <NextLink href="/konseling" passHref>
+            <Button component="a" startIcon={<ArrowBackIcon fontSize="small" />}>
               Perizinan
             </Button>
           </NextLink>
           <form onSubmit={formik.handleSubmit}>
-          <Box sx={{ my: 3 }}>
-              <Typography
-                color="textPrimary"
-                variant="h4"
-              > 
+            <Box sx={{ my: 3 }}>
+              <Typography color="textPrimary" variant="h4">
                 Membuat Data Perizinan
               </Typography>
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="body2"
-              >
+              <Typography color="textSecondary" gutterBottom variant="body2">
                 Silahkan lengkapi data user di bawah ini
               </Typography>
             </Box>
@@ -230,23 +192,24 @@ const clickHandler = (textInput) => {
                 id="demo-simple-select-autowidth"
                 value={formik.values.kelas}
                 onChange={
-                  e => {  formik.handleChange('kelas'); getUsers2(e.target.value); 
-                
-                    }
-                // ; 
-              }
+                  (e) => {
+                    formik.handleChange("kelas");
+                    getUsers2(e.target.value);
+                  }
+                  // ;
+                }
                 autoWidth
                 label="Kelas"
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value={'10-A'}>10-A</MenuItem>
-                <MenuItem value={'10-B'}>10-B</MenuItem>
-                <MenuItem value={'11-A'}>11-A</MenuItem>
-                <MenuItem value={'11-B'}>11-B</MenuItem>
-                <MenuItem value={'12-A'}>12-A</MenuItem>
-                <MenuItem value={'12-B'}>12-B</MenuItem>
+                <MenuItem value={"10-A"}>10-A</MenuItem>
+                <MenuItem value={"10-B"}>10-B</MenuItem>
+                <MenuItem value={"11-A"}>11-A</MenuItem>
+                <MenuItem value={"11-B"}>11-B</MenuItem>
+                <MenuItem value={"12-A"}>12-A</MenuItem>
+                <MenuItem value={"12-B"}>12-B</MenuItem>
               </Select>
             </FormControl>
             <FormControl sx={{ m: 1, minWidth: 80 }}>
@@ -255,14 +218,14 @@ const clickHandler = (textInput) => {
                 labelId="demo-simple-select-autowidth-label"
                 id="demo-simple-select-autowidth"
                 value={formik.values.siswa}
-                onChange={formik.handleChange('siswa')}
+                onChange={formik.handleChange("siswa")}
                 autoWidth
                 label="Siswa"
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                {users.map(item => (
+                {users.map((item) => (
                   <MenuItem value={item.nama}>{item.nama}</MenuItem>
                 ))}
               </Select>
@@ -296,17 +259,17 @@ const clickHandler = (textInput) => {
                 labelId="demo-simple-select-autowidth-label"
                 id="demo-simple-select-autowidth"
                 value={formik.values.status}
-                onChange={formik.handleChange('status')}
+                onChange={formik.handleChange("status")}
                 autoWidth
                 label="Status"
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value={'on pending'}>On Pending</MenuItem>
-                <MenuItem value={'declined'}>Declined</MenuItem>
-                <MenuItem value={'scheduled'}>Scheduled</MenuItem>
-                <MenuItem value={'done'}>Done</MenuItem>
+                <MenuItem value={"on pending"}>On Pending</MenuItem>
+                <MenuItem value={"declined"}>Declined</MenuItem>
+                <MenuItem value={"scheduled"}>Scheduled</MenuItem>
+                <MenuItem value={"done"}>Done</MenuItem>
               </Select>
             </FormControl>
             <FormControl sx={{ m: 1, minWidth: 80 }}>
@@ -315,14 +278,14 @@ const clickHandler = (textInput) => {
                 labelId="demo-simple-select-autowidth-label"
                 id="demo-simple-select-autowidth"
                 value={formik.values.guru}
-                onChange={formik.handleChange('guru')}
+                onChange={formik.handleChange("guru")}
                 autoWidth
                 label="Guru"
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                {users.map(item => (
+                {users2.map((item) => (
                   <MenuItem value={item.nama}>{item.nama}</MenuItem>
                 ))}
               </Select>
@@ -333,24 +296,24 @@ const clickHandler = (textInput) => {
                 labelId="demo-simple-select-autowidth-label"
                 id="demo-simple-select-autowidth"
                 value={formik.values.jam}
-                onChange={formik.handleChange('jam')}
+                onChange={formik.handleChange("jam")}
                 autoWidth
                 label="Jam"
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value={'9.30'}>9.30 - 10.15</MenuItem>
-                <MenuItem value={'10.30'}>10.30 - 11.15</MenuItem>
-                <MenuItem value={'11.30'}>11.30 - 12.15</MenuItem>
-                <MenuItem value={'13.30'}>13.30 - 14.15</MenuItem>
+                <MenuItem value={"9.30"}>9.30 - 10.15</MenuItem>
+                <MenuItem value={"10.30"}>10.30 - 11.15</MenuItem>
+                <MenuItem value={"11.30"}>11.30 - 12.15</MenuItem>
+                <MenuItem value={"13.30"}>13.30 - 14.15</MenuItem>
               </Select>
             </FormControl>
             <Box
               sx={{
-                alignItems: 'center',
-                display: 'flex',
-                ml: -1
+                alignItems: "center",
+                display: "flex",
+                ml: -1,
               }}
             >
               <Checkbox
@@ -358,18 +321,12 @@ const clickHandler = (textInput) => {
                 name="policy"
                 onChange={formik.handleChange}
               />
-              <Typography
-                color="textSecondary"
-                variant="body2"
-              >
+              <Typography color="textSecondary" variant="body2">
                 Saya Menyatakan Bertanggung Jawab Atas Data Yang Saya Masukkan
-                
               </Typography>
             </Box>
             {Boolean(formik.touched.policy && formik.errors.policy) && (
-              <FormHelperText error>
-                {formik.errors.policy}
-              </FormHelperText>
+              <FormHelperText error>{formik.errors.policy}</FormHelperText>
             )}
             <Box sx={{ py: 2 }}>
               <Button
